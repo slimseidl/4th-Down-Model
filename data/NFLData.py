95ded74cb6c6ec5076c49abee34f23c936021c1a
John Seidl <jseidl2@wgu.edu>
2025-06-17 21:21:28 -0500

    Future Proofing NFL Data Python File

--- data/NFLData.py
+++ data/NFLData.py
@@ -1,13 +1,16 @@
 from nfl_data_py import import_pbp_data
 import pandas as pd
+from datetime import datetime
+
+current_year = datetime.now().year
 
 df_list = []
 
-for year in range(2010, 2026):
+for year in range(current_year - 15, current_year - 1):
     print(f"Loading {year} data")
     data = import_pbp_data([year])
     df_list.append(data)
 
 full_df = pd.concat(df_list, ignore_index=True)
 
-full_df.to_csv("data/playbyplay_2010_2025")
+full_df.to_csv(f"data/playbyplay_{current_year - 15}_{current_year - 1}.csv", index=False)
