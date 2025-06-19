91d373e4efe5d4dfda7d8a2f1c3e37c9ab0ee2bb
John Seidl <jseidl2@wgu.edu>
2025-06-18 20:40:38 -0500

    Added comments to pbp file

--- data/NFLData.py
+++ data/NFLData.py
@@ -6,11 +6,14 @@ current_year = datetime.now().year
 
 df_list = []
 
+# Loop over play by play data for last 14 years and append to a csv file
 for year in range(current_year - 15, current_year - 1):
     print(f"Loading {year} data")
     data = import_pbp_data([year])
     df_list.append(data)
 
+
 full_df = pd.concat(df_list, ignore_index=True)
 
+# Converting the data frame to a csv file for later use
 full_df.to_csv(f"data/playbyplay_{current_year - 15}_{current_year - 1}.csv", index=False)
