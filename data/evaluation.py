ef01266e2ec80a6b0e1e3e9dfe043e2e6ba3e8db
John Seidl <jseidl2@wgu.edu>
2025-07-06 23:02:44 -0500

    Commented Code

--- data/evaluation.py
+++ data/evaluation.py
@@ -2,7 +2,8 @@ import pandas as pd
 import sys
 import os
 
-# Project root added to path
+# Modifying sustem path to import decision logic
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app.logic.decision_logic import decide_ev_based, load_decision_table
@@ -14,13 +15,15 @@ df = pd.read_csv("C:/Users/jseidl/NFL-4th-Down/data/processed/cleaned_4th_down.c
 # Loading decision table
 decision_table = load_decision_table("C:/Users/jseidl/NFL-4th-Down/data/processed/decision_table.csv")
 
+# Applying decision to each row
 df['recommended'] = df.apply(lambda row: decide_ev_based(
     row['ydstogo'],
     row['yardline_100'],
     decision_table
 ), axis=1)
 
-# Actual Play Type and Match Status
+# Defining what the coach actually did with function below
+
 def actual_choice(row):
     if row['play_type'] in ['run', 'pass']:
         return "Go"
@@ -31,7 +34,8 @@ def actual_choice(row):
 df['actual'] = df.apply(actual_choice, axis=1)
 df['matched'] = df['recommended'] == df['actual']
 
-# Accuracy Metrics
+# Implementing accuracy metrics to compare coach to model 
+# See Image of accuracy
 match_rate = df['matched'].mean()
 success_when_matched = df[df['matched']]['success'].mean()
 actual_success = df['success'].mean()
@@ -41,4 +45,5 @@ print(f"Tool Match Accuracy: {match_rate:.2%}")
 print(f"Success When Tool Followed: {success_when_matched:.2%}")
 print(f"Overall Success Rate: {actual_success:.2%}")
 
+# Saving full dataset to CSV for future use
 df.to_csv("C:/Users/jseidl/NFL-4th-Down/data/processed/eval_results.csv", index=False)
